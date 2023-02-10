342600d78d14104ca696c936b70c518a2bef0987
kangsh9107 <kangsh9107@naver.com>
2023-02-11 01:24:15 +0900

    2023-02-11 study

--- CodingTest-Java/src/main/java/programmers/MockExam.java
+++ CodingTest-Java/src/main/java/programmers/MockExam.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class MockExam {
 
+	//프로그래머스 Level 1. 모의고사
+	//https://school.programmers.co.kr/learn/courses/30/lessons/42840
 	public static void main(String[] args) {
 		MockExam me = new MockExam();
 		
@@ -19,10 +21,7 @@ public class MockExam {
 		int[] one   = {1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5};
 		int[] two   = {2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5};
 		int[] three = {3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5};
-		int cntOne = 0;
-		int cntTwo = 0;
-		int cntThree = 0;
-		int index = 0;
+		int cntOne = 0, cntTwo = 0, cntThree = 0, index = 0;
 		for(int i=0; i<answers.length; i++) {
 			if(index == 40) index = 0;
 			if(answers[i] == one[index]) cntOne++;
@@ -30,18 +29,16 @@ public class MockExam {
 			if(answers[i] == three[index]) cntThree++;
 			index++;
 		}
-		
 		List<Integer> answer = new ArrayList<>();
-		if(cntOne == 0 && cntTwo == 0 && cntThree == 0) return answer;
 		if(cntOne == cntTwo && cntOne == cntThree) {
 			answer.add(1);
 			answer.add(2);
 			answer.add(3);
-		} else if(cntOne > cntTwo || cntOne > cntThree) {
+		} else if(cntOne > cntTwo && cntOne > cntThree) {
 			answer.add(1);
-		} else if(cntTwo > cntOne || cntTwo > cntThree) {
+		} else if(cntTwo > cntOne && cntTwo > cntThree) {
 			answer.add(2);
-		} else if(cntThree > cntOne || cntThree > cntTwo) {
+		} else if(cntThree > cntOne && cntThree > cntTwo) {
 			answer.add(3);
 		} else if(cntOne == cntTwo && cntOne > cntThree) {
 			answer.add(1);
---
+++ CodingTest-Java/src/main/java/programmers/ProximateString.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProximateString {
+
+	//프로그래머스 Level 1. 가장 가까운 같은 글자
+	//https://school.programmers.co.kr/learn/courses/30/lessons/142086
+	public static void main(String[] args) {
+		ProximateString ps = new ProximateString();
+		
+		String s1 = "banana";
+		System.out.println(ps.solution(s1));
+		
+//		String s2 = "foobar";
+//		System.out.println(ps.solution(s2));
+	}
+	
+	public List<Integer> solution(String s) {
+		Map<String, Integer> hm = new HashMap<>();
+		List<Integer> answer = new ArrayList<>();
+		String temp = "";
+		for(int i=0; i<s.length(); i++) {
+			temp = s.substring(i, i+1);
+			if(hm.get(temp) == null) {
+				hm.put(temp, i);
+				answer.add(-1);
+			} else {
+				answer.add(i - hm.get(temp));
+				hm.put(temp, i);
+			}
+		}
+		return answer;
+	}
+
+}
