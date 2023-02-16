1a06d11b37723db713602df77abc256399bce65b
kangsh9107 <kangsh9107@naver.com>
2023-02-16 23:53:56 +0900

    2023-02-16 study

---
+++ CodingTest-Java/src/main/java/programmers0/ChickenCoupon.java
@@ -0,0 +1,25 @@
+package programmers0;
+
+public class ChickenCoupon {
+
+	//프로그래머스 Level 0. 치킨 쿠폰
+	//https://school.programmers.co.kr/learn/courses/30/lessons/120884
+	public static void main(String[] args) {
+		ChickenCoupon cc = new ChickenCoupon();
+		
+		int chicken1 = 100;
+		System.out.println(cc.solution(chicken1));
+		
+//		int chicken2 = 1081;
+//		System.out.println(cc.solution(chicken2));
+	}
+	
+	public int solution(int chicken) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+
+}
