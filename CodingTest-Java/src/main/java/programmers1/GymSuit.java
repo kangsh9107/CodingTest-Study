b97918c688ab26d19a2c9d03673500c874bc36d3
kangsh9107 <kangsh9107@naver.com>
2023-02-18 03:13:05 +0900

    2023-02-18 study

--- CodingTest-Java/src/main/java/programmers1/GymSuit.java
+++ CodingTest-Java/src/main/java/programmers1/GymSuit.java
@@ -1,7 +1,9 @@
 package programmers1;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class GymSuit {
 
@@ -15,99 +17,45 @@ public class GymSuit {
 		int[] reserve1 = {1,3,5};
 		System.out.println(gs.solution(n1, lost1, reserve1));
 		
-//		int n2 = 5;
-//		int[] lost2 = {2,4};
-//		int[] reserve2 = {3};
-//		System.out.println(gs.solution(n2, lost2, reserve2));
-//		
-//		int n3 = 3;
-//		int[] lost3 = {3};
-//		int[] reserve3 = {1};
-//		System.out.println(gs.solution(n3, lost3, reserve3));
+		int n2 = 5;
+		int[] lost2 = {2,4};
+		int[] reserve2 = {3};
+		System.out.println(gs.solution(n2, lost2, reserve2));
+		
+		int n3 = 3;
+		int[] lost3 = {3};
+		int[] reserve3 = {1};
+		System.out.println(gs.solution(n3, lost3, reserve3));
 	}
 	
 	public int solution(int n, int[] lost, int[] reserve) {
-		int answer = 0;
-		Map<Integer, Integer> currentGymSuit = new HashMap<>();
-		for(int i=1; i<=n; i++) {
-			currentGymSuit.put(i, 1);
+		int answer = n;
+		int[] student = new int[n];
+		for(int i=0; i<n; i++) {              //학생들은 체육복이 1벌씩 있다.
+			student[i] = 1;
 		}
-		for(int i=0; i<reserve.length; i++) {
-			currentGymSuit.put(reserve[i], 2);
+		for(int i=0; i<reserve.length; i++) { //여벌이 있으면 체육복이 2벌씩 있다.
+			student[reserve[i] - 1] = 2;
 		}
-		for(int i=0; i<lost.length; i++) {
-			currentGymSuit.put(lost[i], currentGymSuit.get(lost[i])-1);
-		}
-		
-		for(Integer key : currentGymSuit.keySet()) {
-			if(currentGymSuit.get(key) == 2) {
-				try {
-					if(currentGymSuit.get(key+1) == 0) {
-						currentGymSuit.put(key, 1);
-						currentGymSuit.put(key+1, 1);
-					}
-				} catch(Exception e) {
-					
-				}
-			}
-		}
-		for(Integer key : currentGymSuit.keySet()) {
-			if(currentGymSuit.get(key) == 2) {
-				try {
-					if(currentGymSuit.get(key-1) == 0) {
-						currentGymSuit.put(key, 1);
-						currentGymSuit.put(key-1, 1);
-					}
-				} catch(Exception e) {
-					
-				}
-			}
-		}
-		for(Integer key : currentGymSuit.keySet()) {
-			if(currentGymSuit.get(key) > 0) answer++;
+		for(int i=0; i<lost.length; i++) {    //잃어버리면 체육복 1벌만 잃어 버린다. 즉, 여벌이 있는 학생이 잃어버리면 1벌이 남는다.
+			student[lost[i] - 1]--;
 		}
 		
-		int answer2 = 0;
-		Map<Integer, Integer> currentGymSuit2 = new HashMap<>();
-		for(int i=1; i<=n; i++) {
-			currentGymSuit2.put(i, 1);
-		}
-		for(int i=0; i<reserve.length; i++) {
-			currentGymSuit2.put(reserve[i], 2);
-		}
-		for(int i=0; i<lost.length; i++) {
-			currentGymSuit2.put(lost[i], currentGymSuit2.get(lost[i])-1);
-		}
-
-		for(Integer key : currentGymSuit2.keySet()) {
-			if(currentGymSuit2.get(key) == 2) {
-				try {
-					if(currentGymSuit2.get(key-1) == 0) {
-						currentGymSuit2.put(key, 1);
-						currentGymSuit2.put(key-1, 1);
-					}
-				} catch(Exception e) {
-					
+		for(int i=0; i<n; i++) {
+			if(student[i] == 0) {   //해당 학생이 체육복이 없는 경우 아래 로직 실행.
+				if(i > 0 && student[i-1] == 2) {          //왼쪽에서 오른쪽으로 빌려주는 경우라서 첫번째 학생 제외
+					student[i]++;   //왼쪽에 있는 학생에게 빌림.
+					student[i-1]--; //왼쪽에 있는 학생이 오른쪽에 있는 학생에게 빌려줌.
+				} else if(i < n-1 && student[i+1] == 2) { //오른쪽에서 왼쪽으로 빌려주는 경우라서 마지막 학생 제외
+					student[i]++;   //오른쪽에 있는 학생에게 빌림.
+					student[i+1]--; //오른쪽에 있는 학생이 왼쪽에 있는 학생에게 빌려줌.
+				} else {            //해당 학생이 체육복이 없고, 빌릴 수도 없을 경우.
+					answer--;
 				}
 			}
-		}
-		for(Integer key : currentGymSuit2.keySet()) {
-			if(currentGymSuit2.get(key) == 2) {
-				try {
-					if(currentGymSuit2.get(key+1) == 0) {
-						currentGymSuit2.put(key, 1);
-						currentGymSuit2.put(key+1, 1);
-					}
-				} catch(Exception e) {
-					
-				}
-			}
-		}
-		for(Integer key : currentGymSuit2.keySet()) {
-			if(currentGymSuit2.get(key) > 0) answer2++;
 		}
 		
-		return Math.max(answer, answer2);
+		return answer;
 	}
 
 }
