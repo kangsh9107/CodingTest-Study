0b5b832e73c98004f362fe052176876b50f19127
kangsh9107 <kangsh9107@naver.com>
2023-02-19 23:49:56 +0900

    2023-02-19 study

---
+++ CodingTest-Java/src/main/java/programmers0/OXQuiz.java
@@ -0,0 +1,23 @@
+package programmers0;
+
+public class OXQuiz {
+
+	public static void main(String[] args) {
+		OXQuiz oxq = new OXQuiz();
+		
+		String[] quiz1 = {"3 - 4 = -3", "5 + 6 = 11"};
+		System.out.println(oxq.solution(quiz1));
+		
+		String[] quiz2 = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
+		System.out.println(oxq.solution(quiz2));
+	}
+	
+	public String[] solution(String[] quiz) {
+		String[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
