2b3372c73ac2ff7227177f1e53df4df4c94769f4
kangsh9107 <kangsh9107@naver.com>
2023-02-19 23:57:15 +0900

    2023-02-19 study

--- CodingTest-Java/src/main/java/programmers0/OXQuiz.java
+++ CodingTest-Java/src/main/java/programmers0/OXQuiz.java
@@ -8,16 +8,22 @@ public class OXQuiz {
 		String[] quiz1 = {"3 - 4 = -3", "5 + 6 = 11"};
 		System.out.println(oxq.solution(quiz1));
 		
-		String[] quiz2 = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
-		System.out.println(oxq.solution(quiz2));
+//		String[] quiz2 = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
+//		System.out.println(oxq.solution(quiz2));
 	}
 	
 	public String[] solution(String[] quiz) {
-		String[] answer = {};
-		
-		
-		
-		return answer;
+        String[] answer = new String[quiz.length];
+        int sum = 0;
+        for(int i=0; i<quiz.length; i++) {
+            String[] splitQ = quiz[i].split(" ");
+            if(splitQ[1].equals("+")) sum = Integer.parseInt(splitQ[0]) + Integer.parseInt(splitQ[2]);
+            else sum = Integer.parseInt(splitQ[0]) - Integer.parseInt(splitQ[2]);
+            
+            if(sum == Integer.parseInt(splitQ[4])) answer[i] = "O";
+            else answer[i] = "X";
+        }
+        return answer;
 	}
 
 }
