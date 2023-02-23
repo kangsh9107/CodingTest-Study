ee28af45eefda0b5b059633b9697a299cce95ded
kangsh9107 <kangsh9107@naver.com>
2023-02-23 23:49:45 +0900

    2023-02-23 study

---
+++ CodingTest-Java/src/main/java/programmers0/FiniteDecimal.java
@@ -0,0 +1,29 @@
+package programmers0;
+
+public class FiniteDecimal {
+
+	public static void main(String[] args) {
+		FiniteDecimal fd = new FiniteDecimal();
+		
+		int a1 = 7;
+		int b1 = 20;
+		System.out.println(fd.solution(a1, b1));
+		
+		int a2 = 11;
+		int b2 = 22;
+		System.out.println(fd.solution(a2, b2));
+		
+		int a3 = 12;
+		int b3 = 21;
+		System.out.println(fd.solution(a3, b3));
+	}
+	
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+
+}
