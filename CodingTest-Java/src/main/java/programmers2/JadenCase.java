5ce111ba020cc6a9ac69932d65d211aeded0a255
kangsh9107 <kangsh9107@naver.com>
2023-02-27 23:52:04 +0900

    2023-02-27 study

---
+++ CodingTest-Java/src/main/java/programmers2/JadenCase.java
@@ -0,0 +1,23 @@
+package programmers2;
+
+public class JadenCase {
+
+	public static void main(String[] args) {
+		JadenCase jc = new JadenCase();
+		
+		String s1 = "3people unFollowed me";
+		System.out.println(jc.solution(s1));
+		
+//		String s2 = "for the last week";
+//		System.out.println(jc.solution(s2));
+	}
+	
+    public String solution(String s) {
+        String answer = "";
+        
+        
+        
+        return answer;
+    }
+
+}
