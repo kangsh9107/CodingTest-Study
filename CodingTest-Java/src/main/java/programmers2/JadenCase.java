f2d4b303fede6f6cb226fbeded6e40013eaa02e2
kangsh9107 <kangsh9107@naver.com>
2023-02-27 23:59:46 +0900

    2023-02-27 study

--- CodingTest-Java/src/main/java/programmers2/JadenCase.java
+++ CodingTest-Java/src/main/java/programmers2/JadenCase.java
@@ -13,11 +13,23 @@ public class JadenCase {
 	}
 	
     public String solution(String s) {
-        String answer = "";
-        
-        
-        
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        String[] splitS = s.split(" ");
+        for(int i=0; i<splitS.length; i++) {
+            for(int j=0; j<splitS[i].length(); j++) {
+                if(j == 0) {
+                    try {
+                        answer.append(splitS[i].substring(j, j+1).toUpperCase());
+                    } catch(Exception e) {
+                        answer.append(splitS[i].substring(j, j+1));
+                    }
+                } else {
+                    answer.append(splitS[i].substring(j, j+1).toLowerCase());
+                }
+            }
+            if(i < splitS.length - 1) answer.append(" ");
+        }
+        return answer.toString();
     }
 
 }
