192851f66fafe0e014a9693fcdfe69da6572199b
kangsh9107 <kangsh9107@naver.com>
2023-03-04 23:28:50 +0900

    2023-03-04 study

---
+++ CodingTest-Java/src/main/java/programmers2/NextBiggerNumber.java
@@ -0,0 +1,37 @@
+package programmers2;
+
+public class NextBiggerNumber {
+
+	//프로그래머스 Level 2. 다음 큰 숫자
+	//https://school.programmers.co.kr/learn/courses/30/lessons/12911
+	public static void main(String[] args) {
+		NextBiggerNumber nbn = new NextBiggerNumber();
+		
+		int n1 = 78;
+		System.out.println(nbn.solution(n1));
+		
+//		int n2 = 15;
+//		System.out.println(nbn.solution(n2));
+	}
+	
+    public int solution(int n) {
+        int cntOne = 0;
+        for(int i=0; i<Integer.toBinaryString(n).length(); i++) {
+        	if(Integer.toBinaryString(n).substring(i, i+1).equals("1")) cntOne++;
+        }
+        
+        int cnt = 0;
+        while(true) {
+        	cnt = 0;
+        	n++;
+        	for(int i=0; i<Integer.toBinaryString(n).length(); i++) {
+            	if(Integer.toBinaryString(n).substring(i, i+1).equals("1")) cnt++;
+        	}
+        	
+        	if(cntOne == cnt) break;
+        }
+        
+        return n;
+    }
+
+}
