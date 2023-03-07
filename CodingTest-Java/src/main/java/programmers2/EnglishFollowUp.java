dcabd53975ebaa72ce188ca5b194ba05d0d9251f
kangsh9107 <kangsh9107@naver.com>
2023-03-08 01:00:44 +0900

    2023-03-08	study

--- CodingTest-Java/src/main/java/programmers2/EnglishFollowUp.java
+++ CodingTest-Java/src/main/java/programmers2/EnglishFollowUp.java
@@ -10,30 +10,35 @@ public class EnglishFollowUp {
 	public static void main(String[] args) {
 		EnglishFollowUp efu = new EnglishFollowUp();
 		
-		int n1 = 3;
-		String[] words1 = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
-		System.out.println(efu.solution(n1, words1));
-		
+//		int n1 = 3;
+//		String[] words1 = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+//		System.out.println(efu.solution(n1, words1));
+//		
 //		int n2 = 5;
 //		String[] words2 = {"hello", "observe", "effect", "take", "either", "recognize", "encourage",
 //						   "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
 //		System.out.println(efu.solution(n2, words2));
-//		
-//		int n3 = 2;
-//		String[] words3 = {"hello", "one", "even", "never", "now", "world", "draw"};
-//		System.out.println(efu.solution(n3, words3));
+		
+		int n3 = 2;
+		String[] words3 = {"hello", "one", "even", "never", "now", "world", "draw"};
+		System.out.println(efu.solution(n3, words3));
 	}
 	
 	public int[] solution(int n, String[] words) {
 		int[] answer = new int[2];
-		int cnt = 0;
+		int[] cnt = new int[n];
 		Set<String> checkDuplicated = new HashSet<>();
-		for(int i=0; i<words.length; i++) {
-			if(checkDuplicated.add(words[i])) {
-				cnt++;
-			} else {
+		
+		cnt[0]++;
+		checkDuplicated.add(words[0]);
+		for(int i=1; i<words.length; i++) {
+			cnt[i % n]++;
+			char last = words[i - 1].charAt(words[i - 1].length() - 1);
+			char first = words[i].charAt(0);
+			
+			if( !checkDuplicated.add(words[i]) || last != first ) {
 				answer[0] = (i % n) + 1;
-				answer[1] = cnt;
+				answer[1] = cnt[i % n];
 				break;
 			}
 		}
