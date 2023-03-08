77c34b35b21ef560822453842e829663b635e7a8
kangsh9107 <kangsh9107@naver.com>
2023-03-08 23:34:52 +0900

    2023-03-08 study

---
+++ CodingTest-Java/src/main/java/programmers2/Carpet.java
@@ -0,0 +1,31 @@
+package programmers2;
+
+public class Carpet {
+
+	//프로그래머스 Level 2. 카펫
+	//https://school.programmers.co.kr/learn/courses/30/lessons/42842
+	public static void main(String[] args) {
+		Carpet cp = new Carpet();
+		
+		int brown1 = 10;
+		int yellow1 = 2;
+		System.out.println(cp.solution(brown1, yellow1));
+		
+//		int brown2 = 8;
+//		int yellow2 = 1;
+//		System.out.println(cp.solution(brown2, yellow2));
+//		
+//		int brown3 = 24;
+//		int yellow3 = 24;
+//		System.out.println(cp.solution(brown3, yellow3));
+	}
+	
+	public int[] solution(int brown, int yellow) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
