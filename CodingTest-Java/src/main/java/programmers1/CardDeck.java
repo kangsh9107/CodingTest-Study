46903e1f866084e2632263b0c2264d0c05d6fed4
kangsh9107 <kangsh9107@naver.com>
2023-03-10 23:35:23 +0900

    2023-03-10 study

---
+++ CodingTest-Java/src/main/java/programmers1/CardDeck.java
@@ -0,0 +1,29 @@
+package programmers1;
+
+public class CardDeck {
+
+	//프로그래머스 Level 1. 카드 뭉치
+	//https://school.programmers.co.kr/learn/courses/30/lessons/159994
+	public static void main(String[] args) {
+		CardDeck cd = new CardDeck();
+		
+		String[] cards1 = {"i", "drink", "water"};
+		String[] cards2 = {"want", "to"};
+		String[] goal1 = {"i", "want", "to", "drink", "water"};
+		System.out.println(cd.solution(cards1, cards2, goal1));
+		
+//		String[] cards3 = {"i", "water", "drink"};
+//		String[] cards4 = {"want", "to"};
+//		String[] goal2 = {"i", "want", "to", "drink", "water"};
+//		System.out.println(cd.solution(cards3, cards4, goal2));
+	}
+	
+	public String solution(String[] cards1, String[] cards2, String[] goal) {
+		String answer = "";
+		
+		
+		
+		return answer;
+	}
+
+}
