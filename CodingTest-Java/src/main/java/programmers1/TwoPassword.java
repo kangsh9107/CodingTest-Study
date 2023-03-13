cdf2badf9aa2e5315ffe57116c18cf1f338303bd
kangsh9107 <kangsh9107@naver.com>
2023-03-14 06:33:04 +0900

    2023-03-14 study

--- CodingTest-Java/src/main/java/programmers1/TwoPassword.java
+++ CodingTest-Java/src/main/java/programmers1/TwoPassword.java
@@ -1,5 +1,9 @@
 package programmers1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TwoPassword {
 
 	//프로그래머스 Level 1. 둘만의 암호
@@ -8,22 +12,28 @@ public class TwoPassword {
 		TwoPassword tp = new TwoPassword();
 		
 		String s1 = "aukks";
-		String skip1 = "wdqd";
+		String skip1 = "wbqd";
 		int index1 = 5;
 		System.out.println(tp.solution(s1, skip1, index1));
 	}
 	
 	public String solution(String s, String skip, int index) {
-		String answer = "";
-		String x = "";
+		StringBuilder answer = new StringBuilder();
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		System.out.println(alphabet.charAt(0) - '0');
-		
-		for(int i=0; i<s.length(); i++) {
-			x = s.substring(i, i+1);
+		List<String> skiped = new ArrayList<>(Arrays.asList(alphabet.split("")));
+		for(int i=0; i<skip.length(); i++) {
+			skiped.remove(skip.substring(i, i + 1));
 		}
 		
-		return answer;
+		for(int i=0; i<s.length(); i++) {
+			for(int j=0; j<skiped.size(); j++) {
+				if(s.substring(i, i + 1).equals(skiped.get(j))) {
+					answer.append(skiped.get((j + index) % skiped.size()));
+				}
+			}
+		}
+		
+		return answer.toString();
 	}
 
 }
