d3e23370e0e4f31e8c78b6bab87388617c8fd127
kangsh9107 <kangsh9107@naver.com>
2023-03-16 02:14:23 +0900

    2023-03-16 study

--- CodingTest-Java/src/main/java/programmers1/RecommendNewId.java
+++ CodingTest-Java/src/main/java/programmers1/RecommendNewId.java
@@ -24,11 +24,82 @@ public class RecommendNewId {
 	}
 	
 	public String solution(String new_id) {
-		String answer = "";
+		StringBuilder answer = new StringBuilder();
+		//1단계
+		String capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String reg = "abcdefghijklmnopqrstuvwxyz0123456789-_.";
+		String temp = "";
+		String[] id = new_id.split("");
+		for(int i=0; i<id.length; i++) {
+			if(capital.contains(id[i])) id[i] = id[i].toLowerCase(); 
+		}
+		//2단계
+		for(int i=0; i<id.length; i++) {
+			if(reg.contains(id[i])) answer.append(id[i]);
+		}
+		//3단계
+		id = answer.toString().split("");
+		answer.setLength(0);;
+		answer.append(id[0]);
+		for(int i=1; i<id.length; i++) {
+			if(id[i - 1].equals(".") && id[i].equals(".")) continue;
+			else answer.append(id[i]);
+		}
+		//4단계
+		if(answer.length() == 1 && answer.toString().equals(".")) {
+			answer.setLength(0);
+		} else if(answer.length() == 2) {
+			if(answer.toString().equals("..")) {
+				answer.setLength(0);
+			} else if(answer.toString().substring(0, 1).equals(".")) {
+				temp = answer.toString().substring(1);
+				answer.setLength(0);
+				answer.append(temp);
+			} else if(answer.toString().substring(1).equals(".")) {
+				temp = answer.toString().substring(0, 1);
+				answer.setLength(0);
+				answer.append(temp);
+			}
+		} else {
+			if(answer.toString().substring(0, 1).equals(".") && answer.toString().substring(answer.length() - 1).equals(".")) {
+				temp = answer.toString().substring(1);
+				temp = temp.substring(0, temp.length() - 1);
+				answer.setLength(0);
+				answer.append(temp);
+			} else if(answer.toString().substring(0, 1).equals(".")) {
+				temp = answer.toString().substring(1);
+				answer.setLength(0);
+				answer.append(temp);
+			} else if(answer.toString().substring(answer.length() - 1).equals(".")) {
+				temp = answer.toString().substring(0, answer.length() - 1);
+				answer.setLength(0);
+				answer.append(temp);
+			}
+		}
+		//5단계
+		if(answer.length() == 0) answer.append("a");
+		//6단계
+		if(answer.length() > 15) {
+			temp = answer.toString().substring(0, 15);
+			answer.setLength(0);
+			answer.append(temp);
+			if(answer.toString().substring(14).equals(".")) {
+				temp = answer.toString().substring(0, 14);
+				answer.setLength(0);
+				answer.append(temp);
+			}
+		}
+		//7단계
+		if(answer.length() == 2) {
+			temp = answer.toString().substring(answer.length() - 1);
+			answer.append(temp);
+		} else if(answer.length() == 1) {
+			temp = answer.toString();
+			answer.append(temp);
+			answer.append(temp);
+		}
 		
-		
-		
-		return answer;
+		return answer.toString();
 	}
 
 }
