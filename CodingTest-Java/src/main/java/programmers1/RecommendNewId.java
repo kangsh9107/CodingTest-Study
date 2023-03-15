b102fb498aad74cd614a8bff989d8255050b6377
kangsh9107 <kangsh9107@naver.com>
2023-03-15 23:36:18 +0900

    2023-03-15 study

---
+++ CodingTest-Java/src/main/java/programmers1/RecommendNewId.java
@@ -0,0 +1,34 @@
+package programmers1;
+
+public class RecommendNewId {
+
+	//프로그래머스 Level 1. 신규 아이디 추천
+	//https://school.programmers.co.kr/learn/courses/30/lessons/72410
+	public static void main(String[] args) {
+		RecommendNewId rni = new RecommendNewId();
+		
+		String new_id1 = "...!@BaT#*..y.abcdefghijklm";
+		System.out.println(rni.solution(new_id1));
+		
+//		String new_id2 = "z-+.^.";
+//		System.out.println(rni.solution(new_id2));
+//		
+//		String new_id3 = "=.=";
+//		System.out.println(rni.solution(new_id3));
+//		
+//		String new_id4 = "123_.def";
+//		System.out.println(rni.solution(new_id4));
+//		
+//		String new_id5 = "abcdefghijklmn.p";
+//		System.out.println(rni.solution(new_id5));
+	}
+	
+	public String solution(String new_id) {
+		String answer = "";
+		
+		
+		
+		return answer;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 프로그래머스
 
-| 문제 난이도 | 문제 링크 | 해결한 문제 | 도전 문제 | 해결비율(%) |
+| 문제 난이도 | 문제 링크 | 해결한 문제 | 도전한 문제 | 해결비율(%) |
 | :--: |:--: |:--: |:--: |:--: |
 |Level 0.|[바로가기](https://github.com/kangsh9107/CodingTest-Study/blob/main/CodingTest-Java/Level0.md)|96|100|96%|
 |Level 1.|[바로가기](https://github.com/kangsh9107/CodingTest-Study/blob/main/CodingTest-Java/Level1.md)|69|73|94%|
