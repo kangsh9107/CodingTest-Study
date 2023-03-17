005882279cababb3a6fe7f0d7868416c7d831696
kangsh9107 <kangsh9107@naver.com>
2023-03-17 23:21:55 +0900

    2023-03-17 study

---
+++ CodingTest-Java/src/main/java/programmers1/ExpirationPeriod.java
@@ -0,0 +1,29 @@
+package programmers1;
+
+public class ExpirationPeriod {
+
+	//프로그래머스 Level 1. 개인정보 수집 유효기간
+	//https://school.programmers.co.kr/learn/courses/30/lessons/150370
+	public static void main(String[] args) {
+		ExpirationPeriod ep = new ExpirationPeriod();
+		
+		String today1 = "2022.05.19";
+		String[] terms1 = {"A 6", "B 12", "C 3"};
+		String[] privacies1 = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+		System.out.println(ep.solution(today1, terms1, privacies1));
+		
+//		String today2 = "2020.01.01";
+//		String[] terms2 = {"Z 3", "D 5"};
+//		String[] privacies2 = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+//		System.out.println(ep.solution(today2, terms2, privacies2));
+	}
+	
+	public int[] solution(String today, String[] terms, String[] privacies) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
