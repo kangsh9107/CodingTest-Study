dec5564bea456b9ce56a23469f8fa2e5e8ee2c99
kangsh9107 <kangsh9107@naver.com>
2023-04-03 23:50:09 +0900

    2023-04-03 study

--- CodingTest-Java/src/main/java/programmers0/FiniteDecimal.java
+++ CodingTest-Java/src/main/java/programmers0/FiniteDecimal.java
@@ -2,6 +2,8 @@ package programmers0;
 
 public class FiniteDecimal {
 
+	//프로그래머스 Level 0. 유한소수 판별하기
+	//https://school.programmers.co.kr/learn/courses/30/lessons/120878
 	public static void main(String[] args) {
 		FiniteDecimal fd = new FiniteDecimal();
 		
@@ -9,13 +11,13 @@ public class FiniteDecimal {
 		int b1 = 20;
 		System.out.println(fd.solution(a1, b1));
 		
-		int a2 = 11;
-		int b2 = 22;
-		System.out.println(fd.solution(a2, b2));
-		
-		int a3 = 12;
-		int b3 = 21;
-		System.out.println(fd.solution(a3, b3));
+//		int a2 = 11;
+//		int b2 = 22;
+//		System.out.println(fd.solution(a2, b2));
+//		
+//		int a3 = 12;
+//		int b3 = 21;
+//		System.out.println(fd.solution(a3, b3));
 	}
 	
     public int solution(int a, int b) {
