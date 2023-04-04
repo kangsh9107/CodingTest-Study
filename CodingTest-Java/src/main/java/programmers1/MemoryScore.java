7de62b5222e43dbed10476d119f672213178bae5
kangsh9107 <kangsh9107@naver.com>
2023-04-04 22:57:30 +0900

    2023-04-04 study

---
+++ CodingTest-Java/src/main/java/programmers1/MemoryScore.java
@@ -0,0 +1,24 @@
+package programmers1;
+
+public class MemoryScore {
+
+	//프로그래머스 Level 1. 추억 점수
+	//https://school.programmers.co.kr/learn/courses/30/lessons/176963
+	public static void main(String[] args) {
+		MemoryScore ms = new MemoryScore();
+		
+		String[] name1 = {"may", "kein", "kain", "radi"};
+		int[] yearning1 = {5, 10, 1, 3};
+		String[][] photo1 = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		System.out.println(ms.solution(name1, yearning1, photo1));
+	}
+	
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
