a25aecfb8802a651637e0109b58408fca9bb80a5
kangsh9107 <kangsh9107@naver.com>
2023-04-07 23:30:01 +0900

    2023-04-07 study

---
+++ CodingTest-Java/src/main/java/programmers1/RunningRace.java
@@ -0,0 +1,23 @@
+package programmers1;
+
+public class RunningRace {
+
+	//프로그래머스 Level 1. 달리기 경주
+	//https://school.programmers.co.kr/learn/courses/30/lessons/178871
+	public static void main(String[] args) {
+		RunningRace rr = new RunningRace();
+		
+		String[] players1 = {"mumu", "soe", "poe", "kai", "mine"};
+		String[] callings1 = {"kai", "kai", "mine", "mine"};
+		System.out.println(rr.solution(players1, callings1));
+	}
+	
+	public String[] solution(String[] players, String[] callings) {
+		String[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
