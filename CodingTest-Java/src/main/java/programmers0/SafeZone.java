e11614f04849af8a7eb41f935b17adb9f46d3624
kangsh9107 <kangsh9107@naver.com>
2023-04-20 23:58:12 +0900

    2023-04-20 study

---
+++ CodingTest-Java/src/main/java/programmers0/SafeZone.java
@@ -0,0 +1,22 @@
+package programmers0;
+
+public class SafeZone {
+
+	//프로그래머스 Level 0. 안전지대
+	//https://school.programmers.co.kr/learn/courses/30/lessons/120866
+	public static void main(String[] args) {
+		SafeZone sz = new SafeZone();
+		
+		int[][] board1 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+		System.out.println(sz.solution(board1));
+	}
+	
+	public int solution(int[][] board) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+
+}
