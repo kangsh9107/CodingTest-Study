ed8064816dab86c5aff8c0134e3fa7312832766e
kangsh9107 <kangsh9107@naver.com>
2023-04-23 23:42:58 +0900

    2023-04-23 study

--- CodingTest-Java/src/main/java/programmers0/SafeZone.java
+++ CodingTest-Java/src/main/java/programmers0/SafeZone.java
@@ -20,17 +20,27 @@ public class SafeZone {
 	public int solution(int[][] board) {
 		int answer = 0;
 		
+		//지뢰 체크
 		int[][] map = new int[board.length][board.length];
-		boolean[][] visited = new boolean[board.length][board.length];
 		for(int i=0; i<board.length; i++) {
 			for(int j=0; j<board.length; j++) {
-				map[i][j] = board[i][j];
-				System.out.print(map[i][j]);
+				if(map[i][j] == 1) bomb(i, j, map);
 			}
-			System.out.println();
+		}
+		
+		//안전지대 카운팅
+		for(int i=0; i<map.length; i++) {
+			for(int j=0; j<map.length; j++) {
+				if(map[i][j] == 0) answer++;
+			}
 		}
 		
 		return answer;
+	}
+	
+	//지뢰 표시
+	public void bomb(int i, int j, int[][] map) {
+		
 	}
 
 }
