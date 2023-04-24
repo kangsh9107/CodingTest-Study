61c5f44a33a8cee4639a012f1acd79265ce0af08
kangsh9107 <kangsh9107@naver.com>
2023-04-24 23:43:11 +0900

    2023-04-24 study

---
+++ CodingTest-Java/src/main/java/programmers0/SpecialTwoDimension.java
@@ -0,0 +1,22 @@
+package programmers0;
+
+public class SpecialTwoDimension {
+
+	//프로그래머스 Level 0. 특별한 이차원 배열 2
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181831
+	public static void main(String[] args) {
+		SpecialTwoDimension std = new SpecialTwoDimension();
+		
+		int[][] arr1 = {{5, 192, 33}, {192, 72, 95}, {33, 95, 999}};
+		System.out.println(std.solution(arr1));
+	}
+	
+	public int solution(int[][] arr) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+
+}
