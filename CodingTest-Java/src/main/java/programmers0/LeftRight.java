00cbfe8009f062a3d368545841e6ca8cbffc2016
kangsh9107 <kangsh9107@naver.com>
2023-05-21 23:51:42 +0900

    2023-05-21 study

---
+++ CodingTest-Java/src/main/java/programmers0/LeftRight.java
@@ -0,0 +1,25 @@
+package programmers0;
+
+public class LeftRight {
+
+	//프로그래머스 Level 0. 왼쪽 오른쪽
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181890
+	public static void main(String[] args) {
+		LeftRight lr = new LeftRight();
+		
+		String[] str_list1 = {"u", "u", "l", "r"};
+		System.out.println(lr.solution(str_list1));
+		
+//		String[] str_list2 = {"l"};
+//		System.out.println(lr.solution(str_list2));
+	}
+
+	private String[] solution(String[] str_list) {
+		String[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
