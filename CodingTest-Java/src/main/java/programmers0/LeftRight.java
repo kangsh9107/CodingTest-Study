1eb789e7ee272c57e6814118464bc1a5dda0d4e2
kangsh9107 <kangsh9107@naver.com>
2023-05-22 00:11:31 +0900

    2023-05-22 study

--- CodingTest-Java/src/main/java/programmers0/LeftRight.java
+++ CodingTest-Java/src/main/java/programmers0/LeftRight.java
@@ -1,5 +1,8 @@
 package programmers0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeftRight {
 
 	//프로그래머스 Level 0. 왼쪽 오른쪽
@@ -7,17 +10,40 @@ public class LeftRight {
 	public static void main(String[] args) {
 		LeftRight lr = new LeftRight();
 		
-		String[] str_list1 = {"u", "u", "l", "r"};
-		System.out.println(lr.solution(str_list1));
+//		String[] str_list1 = {"u", "u", "l", "r"};
+//		System.out.println(lr.solution(str_list1));
 		
-//		String[] str_list2 = {"l"};
-//		System.out.println(lr.solution(str_list2));
+		String[] str_list2 = {"l"};
+		System.out.println(lr.solution(str_list2));
 	}
 
-	private String[] solution(String[] str_list) {
-		String[] answer = {};
+	private List<String> solution(String[] str_list) {
+		List<String> answer = new ArrayList<>();
+		String dir = "";
+		int idx = 0;
+		for (int i=0; i<str_list.length; i++) {
+			if(str_list[i].equals("l")) {
+				dir = "l";
+				idx = i;
+				break;
+			} else if (str_list[i].equals("r")) {
+				dir = "r";
+				idx = i;
+				break;
+			}
+		}
 		
-		
+		if (str_list.length == 0) {
+			
+		} else if (dir.equals("l")) {
+			for (int i=0; i<idx; i++) {
+				answer.add(str_list[i]);
+			}
+		} else {
+			for (int i=idx+1; i<str_list.length; i++) {
+				answer.add(str_list[i]);
+			}
+		}
 		
 		return answer;
 	}
