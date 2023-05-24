39ca4c40526a08959a84dc16913527de4fa96285
kangsh9107 <kangsh9107@naver.com>
2023-05-24 19:00:56 +0900

    2023-05-24 study

---
+++ CodingTest-Java/src/main/java/programmers0/SculptingAnArray.java
@@ -0,0 +1,23 @@
+package programmers0;
+
+public class SculptingAnArray {
+
+	//프로그래머스 Level 0. 배열 조각하기
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181893
+	public static void main(String[] args) {
+		SculptingAnArray saa = new SculptingAnArray();
+		
+		int[] arr1 = {0, 1, 2, 3, 4, 5};
+		int[] query1 = {4, 1, 2};
+		System.out.println(saa.solution(arr1, query1));
+	}
+
+	private int[] solution(int[] arr, int[] query) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
