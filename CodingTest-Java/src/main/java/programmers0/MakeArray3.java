5518746a19352c07dc2820cc9593d5c58a53efd4
kangsh9107 <kangsh9107@naver.com>
2023-06-02 00:04:50 +0900

    2023-06-02 study

--- CodingTest-Java/src/main/java/programmers0/MakeArray3.java
+++ CodingTest-Java/src/main/java/programmers0/MakeArray3.java
@@ -1,5 +1,8 @@
 package programmers0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeArray3 {
 
 	//프로그래머스 Level 0. 배열 만들기 3
@@ -8,13 +11,18 @@ public class MakeArray3 {
 		MakeArray3 ma = new MakeArray3();
 		
 		int[] arr1 = {1, 2, 3, 4, 5};
-		System.out.println(ma.solution(arr1));
+		int[][] intervals1 = {{1, 3}, {0, 4}};
+		System.out.println(ma.solution(arr1, intervals1));
 	}
 
-	private int[] solution(int[] arr) {
-		int[] answer = {};
+	private List<Integer> solution(int[] arr, int[][] intervals) {
+		List<Integer> answer = new ArrayList<>();
 		
-		
+		for (int i=0; i<intervals.length; i++) {
+			for (int j=intervals[i][0]; j<=intervals[i][1]; j++) {
+				answer.add(arr[j]);
+			}
+		}
 		
 		return answer;
 	}
