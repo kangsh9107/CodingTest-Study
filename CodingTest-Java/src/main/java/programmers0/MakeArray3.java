0c2c6209219a32c160c501bb96648271974cdc97
kangsh9107 <kangsh9107@naver.com>
2023-06-01 23:49:26 +0900

    2023-06-01 study

---
+++ CodingTest-Java/src/main/java/programmers0/MakeArray3.java
@@ -0,0 +1,22 @@
+package programmers0;
+
+public class MakeArray3 {
+
+	//프로그래머스 Level 0. 배열 만들기 3
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181895
+	public static void main(String[] args) {
+		MakeArray3 ma = new MakeArray3();
+		
+		int[] arr1 = {1, 2, 3, 4, 5};
+		System.out.println(ma.solution(arr1));
+	}
+
+	private int[] solution(int[] arr) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
