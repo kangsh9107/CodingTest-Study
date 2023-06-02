7231d1d82de7ead85e19b9ff711e8fc1a1d60ef6
kangsh9107 <kangsh9107@naver.com>
2023-06-02 23:41:59 +0900

    2023-06-02 study

---
+++ CodingTest-Java/src/main/java/programmers0/FirstNegativeNumber.java
@@ -0,0 +1,30 @@
+package programmers0;
+
+import java.util.Arrays;
+
+public class FirstNegativeNumber {
+
+	//프로그래머스 Level 0. 첫 번째로 나오는 음수
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181896
+	public static void main(String[] args) {
+		FirstNegativeNumber fnn = new FirstNegativeNumber();
+		
+		int[] num_list1 = {12, 4, 15, 46, 38, -2, 15};
+		System.out.println(fnn.solution(num_list1));
+		
+//		int[] num_list2 = {13, 22, 53, 24, 15, 6};
+//		System.out.println(fnn.solution(num_list2));
+	}
+
+	private int solution(int[] num_list) {
+		Arrays.sort(num_list);
+		
+		if (num_list[0] >= 0) {
+			return -1;
+		} else {
+			
+			return 0;
+		}
+	}
+
+}
