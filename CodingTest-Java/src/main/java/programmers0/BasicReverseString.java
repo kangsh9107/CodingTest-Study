9d1aa84cde95f9964009d720e1bfeab29f74ef1c
kangsh9107 <kangsh9107@naver.com>
2023-06-06 23:52:36 +0900

    2023-06-06 study

---
+++ CodingTest-Java/src/main/java/programmers0/BasicReverseString.java
@@ -0,0 +1,27 @@
+package programmers0;
+
+public class BasicReverseString {
+
+	//프로그래머스 Level 0. 문자열 뒤집기
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181905
+	public static void main(String[] args) {
+		BasicReverseString brs = new BasicReverseString();
+		
+		String my_string1 = "Progra21Sremm3";
+		int s1 = 6;
+		int e1 = 12;
+		System.out.println(brs.solution(my_string1, s1, e1));
+		
+//		String my_string2 = "Stanley1yelnatS";
+//		int s2 = 4;
+//		int e2 = 10;
+//		System.out.println(brs.solution(my_string2, s2, e2));
+	}
+
+	private String solution(String my_string, int s, int e) {
+		StringBuilder answer = new StringBuilder();
+		answer.append(my_string.substring(s, e + 1)).reverse();
+		return my_string.substring(0, s) + answer.toString();
+	}
+
+}
