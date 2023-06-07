6efa15b4b9bcc2f32efd071a18a42adafb008b18
kangsh9107 <kangsh9107@naver.com>
2023-06-07 23:07:30 +0900

    2023-06-07 study

---
+++ CodingTest-Java/src/main/java/programmers0/CheckIfPrefix.java
@@ -0,0 +1,35 @@
+package programmers0;
+
+public class CheckIfPrefix {
+
+	//프로그래머스 Level 0. 접두사인지 확인하기
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181906
+	public static void main(String[] args) {
+		CheckIfPrefix cip = new CheckIfPrefix();
+		
+		String my_string1 = "banana";
+		String is_prefix1 = "ban";
+		System.out.println(cip.solution(my_string1, is_prefix1));
+		
+//		String my_string2 = "banana";
+//		String is_prefix2 = "nan";
+//		System.out.println(cip.solution(my_string2, is_prefix2));
+//		
+//		String my_string3 = "banana";
+//		String is_prefix3 = "abcd";
+//		System.out.println(cip.solution(my_string3, is_prefix3));
+//		
+//		String my_string4 = "banana";
+//		String is_prefix4 = "bananan";
+//		System.out.println(cip.solution(my_string4, is_prefix4));
+	}
+
+	private int solution(String my_string, String is_prefix) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+
+}
