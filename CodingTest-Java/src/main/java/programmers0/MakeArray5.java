f85c9463940efcc484973637f4eaaf4c6170f723
kangsh9107 <kangsh9107@naver.com>
2023-06-10 21:23:04 +0900

    2023-06-10 study

--- CodingTest-Java/src/main/java/programmers0/MakeArray5.java
+++ CodingTest-Java/src/main/java/programmers0/MakeArray5.java
@@ -1,5 +1,8 @@
 package programmers0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeArray5 {
 
 	//프로그래머스 Level 0. 배열 만들기 5
@@ -14,10 +17,13 @@ public class MakeArray5 {
 		System.out.println(ma.solution(intStrs1, k1, s1, l1));
 	}
 
-	private int[] solution(String[] intStrs, int k, int s, int l) {
-		int[] answer = {};
+	private List<Integer> solution(String[] intStrs, int k, int s, int l) {
+		List<Integer> answer = new ArrayList<>();
 		
-		
+		for (int i=0; i<intStrs.length; i++) {
+			int num = Integer.parseInt(intStrs[i].substring(s, s + l));
+			if (num > k) answer.add(num);
+		}
 		
 		return answer;
 	}
---
+++ CodingTest-Java/src/main/java/programmers0/ReverseAStringMultipleTimes.java
@@ -0,0 +1,23 @@
+package programmers0;
+
+public class ReverseAStringMultipleTimes {
+
+	//프로그래머스 Level 0. 문자열 여러 번 뒤집기
+	//https://school.programmers.co.kr/learn/courses/30/lessons/181913
+	public static void main(String[] args) {
+		ReverseAStringMultipleTimes rasmt = new ReverseAStringMultipleTimes();
+		
+		String my_string1 = "rermgorpsam";
+		int[][] queries = {{2, 3}, {0, 7}, {5, 9}, {6, 10}};
+		System.out.println(rasmt.solution(my_string1));
+	}
+
+	private String solution(String my_string) {
+		StringBuilder answer = new StringBuilder();
+		
+		
+		
+		return answer.toString();
+	}
+
+}
