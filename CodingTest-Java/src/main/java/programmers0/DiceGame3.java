837bd8344c4c9e4a04543f94322ac24e11efa82c
kangsh9107 <kangsh9107@naver.com>
2023-06-14 23:49:42 +0900

    2023-06-14 study

--- CodingTest-Java/src/main/java/programmers0/DiceGame3.java
+++ CodingTest-Java/src/main/java/programmers0/DiceGame3.java
@@ -1,5 +1,8 @@
 package programmers0;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DiceGame3 {
 
 	//프로그래머스 Level 0. 주사위 게임 3
@@ -39,9 +42,30 @@ public class DiceGame3 {
 	}
 
 	private int solution(int a, int b, int c, int d) {
+		Set<Integer> hs = new HashSet<>();
+		int[] numbers = new int[4];
+		numbers[0] = a;
+		numbers[1] = b;
+		numbers[2] = c;
+		numbers[3] = d;
 		int answer = 0;
+		int cnt = 0;
 		
+		for (int i=0; i<numbers.length; i++) {
+			if (hs.add(numbers[i])) {
+				
+			} else {
+				cnt++;
+			}
+		}
 		
+		if (cnt == 3) {
+			answer = 1111 * a;
+		} else if (cnt == 2) {
+			
+		} else if (cnt == 2) {
+			
+		}
 		
 		return answer;
 	}
