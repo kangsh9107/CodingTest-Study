05c417a6b58ffdd685f790a323fb76b86fe1ae5a
kangsh9107 <kangsh9107@naver.com>
2023-06-21 23:27:49 +0900

    2023-06-21 study

---
+++ CodingTest-Java/src/main/java/programmers0/ConditionString.java
@@ -0,0 +1,44 @@
+package main.java.programmers0;
+
+public class ConditionString {
+
+    /*
+     * 프로그래머스 Level 0. 코드 처리하기
+     * https://school.programmers.co.kr/learn/courses/30/lessons/181932
+     *
+     * 생성일 : 2023-06-21
+     * 완료일 : 2023-06-21
+     */
+    public static void main(String[] args) {
+        ConditionString sol = new ConditionString();
+
+        String ineq1 = "<";
+        String eq1 = "=";
+        int n1 = 20;
+        int m1 = 50;
+        System.out.println(sol.solution(ineq1, eq1, n1, m1));
+
+//        String ineq2 = ">";
+//        String eq2 = "!";
+//        int n2 = 41;
+//        int m2 = 78;
+//        System.out.println(sol.solution(ineq2, eq2, n2, m2));
+    }
+
+    private int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+
+        if (ineq.equals(">") && eq.equals("=")) {
+            answer = n >= m ? 1 : 0;
+        } else if (ineq.equals(">") && eq.equals("!")) {
+            answer = n > m ? 1 : 0;
+        } else if (ineq.equals("<") && eq.equals("=")) {
+            answer = n <= m ? 1 : 0;
+        } else {
+            answer = n < m ? 1 : 0;
+        }
+
+        return answer;
+    }
+
+}
