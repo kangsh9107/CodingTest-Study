127b7701c42d2829cf44f5a186546fff535a29e3
kangsh9107 <kangsh9107@naver.com>
2023-06-24 00:52:51 +0900

    2023-06-24 study

---
+++ CodingTest-Java/src/main/java/programmers0/SumOfTwoNumbers.java
@@ -0,0 +1,36 @@
+package main.java.programmers0;
+
+public class SumOfTwoNumbers {
+
+    /*
+     * 프로그래머스 Level 0. 두 수의 합
+     * https://school.programmers.co.kr/learn/courses/30/lessons/181846
+     *
+     * 생성일 : 2023-06-23
+     * 완료일 : 2023-06-23
+     */
+    public static void main(String[] args) {
+        SumOfTwoNumbers sol = new SumOfTwoNumbers();
+
+        String a1 = "582";
+        String b1 = "734";
+        System.out.println(sol.solution(a1, b1));
+
+//        String a2 = "18446744073709551615";
+//        String b2 = "287346502836570928366";
+//        System.out.println(sol.solution(a2, b2));
+//
+//        String a3 = "0";
+//        String b3 = "0";
+//        System.out.println(sol.solution(a3, b3));
+    }
+
+    private String solution(String a, String b) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+}
