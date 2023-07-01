842e2aa54e883a72c8b1857a441a9693d95ca079
kangsh9107 <kangsh9107@naver.com>
2023-07-01 23:25:32 +0900

    2023-07-01 study

--- CodingTest-Java/src/main/java/programmers0/DiceGame3.java
+++ CodingTest-Java/src/main/java/programmers0/DiceGame3.java
@@ -1,16 +1,17 @@
 package programmers0;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.annotation.processing.SupportedSourceVersion;
 
 public class DiceGame3 {
 
-	//프로그래머스 Level 0. 주사위 게임 3
-	//https://school.programmers.co.kr/learn/courses/30/lessons/181916
+	/*
+	 * 프로그래머스 Level 0. 주사위 게임 3
+	 * https://school.programmers.co.kr/learn/courses/30/lessons/181916
+	 *
+	 * 생성일 : 2023-06-26
+	 * 완료일 : 2023-06-
+	 */
 	public static void main(String[] args) {
 		DiceGame3 dg = new DiceGame3();
 		
@@ -46,12 +47,12 @@ public class DiceGame3 {
 	}
 
 	private int solution(int a, int b, int c, int d) {
-		int answer = 0;
 		int[] nums = new int[4];
 		nums[0] = a;
-		nums[1] = b;
-		nums[2] = c;
-		nums[3] = d;
+		nums[0] = b;
+		nums[0] = c;
+		nums[0] = d;
+
 		Map<Integer, Integer> hm = new HashMap<>();
 		for (int i=0; i<nums.length; i++) {
 			if (hm.get(nums[i]) == null) {
@@ -60,33 +61,33 @@ public class DiceGame3 {
 				hm.put(nums[i], hm.get(nums[i]) + 1);
 			}
 		}
-		
-		List<Integer> isDup = new ArrayList<>();
-		List<Integer> isNotDup = new ArrayList<>();
-		int min = 7;
+
+		int answer = 0;
+		int p = 0;
+		int q = 0;
+		int cntDup = 0;
 		for (int key : hm.keySet()) {
-			min = Math.min(hm.get(key), min);
-			
-			if (hm.get(key) != 1) {
-				isDup.add(key);
-			} else {
-				isNotDup.add(key);
-			}
+			cntDup = Math.max(cntDup, hm.get(key));
 		}
-		
-		if (hm.size() == 1) {
-			answer = 1111 * isDup.get(0);
-		} else if (hm.size() == 2 && isDup.size() == 1) {
-			answer = (int) Math.pow(10 * isDup.get(0) + isNotDup.get(0), 2);
-		} else if (hm.size() == 2 && isDup.size() == 2) {
-			answer = (isDup.get(0) + isDup.get(1)) * Math.abs(isDup.get(0) - isDup.get(1));
-		} else if (hm.size() == 3) {
-			answer = isNotDup.get(0) * isNotDup.get(1);
-		} else {
-			answer = min;
+
+
+		if (cntDup == 1) {
+
+		} else if (cntDup == 2) {
+
+		} else if (cntDup == 3) {
+
+		} else if (cntDup == 4) {
+
 		}
-		
+
 		return answer;
+	}
+
+	private void repeat(Map<Integer, Integer> hm, int cntDup) {
+		for (int key : hm.keySet()) {
+
+		}
 	}
 
 }
