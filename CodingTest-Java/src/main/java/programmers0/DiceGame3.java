2ae6752bea2c41c07c772370672f2d1ad8de5c85
kangsh9107 <kangsh9107@naver.com>
2023-07-02 21:47:45 +0900

    2023-07-02 study

--- CodingTest-Java/src/main/java/programmers0/DiceGame3.java
+++ CodingTest-Java/src/main/java/programmers0/DiceGame3.java
@@ -1,7 +1,6 @@
 package programmers0;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DiceGame3 {
 
@@ -10,7 +9,7 @@ public class DiceGame3 {
 	 * https://school.programmers.co.kr/learn/courses/30/lessons/181916
 	 *
 	 * 생성일 : 2023-06-26
-	 * 완료일 : 2023-06-
+	 * 완료일 : 2023-07-02
 	 */
 	public static void main(String[] args) {
 		DiceGame3 dg = new DiceGame3();
@@ -47,47 +46,62 @@ public class DiceGame3 {
 	}
 
 	private int solution(int a, int b, int c, int d) {
-		int[] nums = new int[4];
-		nums[0] = a;
-		nums[0] = b;
-		nums[0] = c;
-		nums[0] = d;
-
-		Map<Integer, Integer> hm = new HashMap<>();
-		for (int i=0; i<nums.length; i++) {
-			if (hm.get(nums[i]) == null) {
-				hm.put(nums[i], 1);
-			} else {
-				hm.put(nums[i], hm.get(nums[i]) + 1);
-			}
+		List<Integer> dice = Arrays.asList(a, b, c, d);
+		Map<Integer, Integer> frequency = new HashMap<>();
+		for (int num : dice) {
+			frequency.put(num, frequency.getOrDefault(num, 0) + 1);
 		}
 
 		int answer = 0;
 		int p = 0;
 		int q = 0;
-		int cntDup = 0;
-		for (int key : hm.keySet()) {
-			cntDup = Math.max(cntDup, hm.get(key));
-		}
+		int r = 0;
+		int cnt = 0;
+		if (frequency.size() == 1) {
+			for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+				p = entry.getKey();
+				answer = 1111 * p;
+			}
+		} else if (frequency.size() == 2) {
+			for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+				cnt = Math.max(cnt, entry.getValue());
+			}
 
-
-		if (cntDup == 1) {
-
-		} else if (cntDup == 2) {
-
-		} else if (cntDup == 3) {
-
-		} else if (cntDup == 4) {
-
+			if (cnt == 3) {
+				for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+					if (entry.getValue() == 3) {
+						p = entry.getKey();
+					} else {
+						q = entry.getKey();
+					}
+				}
+				answer = (int) Math.pow((10 * p) + q, 2);
+			} else {
+				for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+					if (p == 0) {
+						p = entry.getKey();
+					} else if (p != 0 && p != entry.getKey()) {
+						q = entry.getKey();
+					}
+				}
+				answer = (p + q) * Math.abs(p - q);
+			}
+		} else if (frequency.size() == 3) {
+			for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+				if (entry.getValue() == 1) {
+					if (q == 0) {
+						q = entry.getKey();
+					} else {
+						r = entry.getKey();
+					}
+				}
+			}
+			answer = q * r;
+		} else {
+			answer = Collections.min(dice);
 		}
 
 		return answer;
-	}
-
-	private void repeat(Map<Integer, Integer> hm, int cntDup) {
-		for (int key : hm.keySet()) {
-
-		}
 	}
 
 }
