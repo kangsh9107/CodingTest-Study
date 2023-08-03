2b86f1695b493158c2a94f97e417e245e9d4e739
kangsh9107 <kangsh9107@naver.com>
2023-08-03 23:56:26 +0900

    2023-08-03 23:56 1. 프로그래머스 Level 2. 행렬의 곱셈 https://school.programmers.co.kr/learn/courses/30/lessons/12949

--- CodingTest-Java/src/main/java/programmers2/MatrixMultiplication.java
+++ CodingTest-Java/src/main/java/programmers2/MatrixMultiplication.java
@@ -24,9 +24,6 @@ public class MatrixMultiplication {
     private int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for (int i = 0; i < arr1.length; i++) {
-
-        }
 
         return answer;
     }
