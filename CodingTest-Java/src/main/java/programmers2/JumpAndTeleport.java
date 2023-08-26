a585f832d1647f6ab370166c99bd6047690122b2
KangSuHyeong <xhdrhfdk@gmail.com>
2023-08-26 23:58:18 +0900

    2023-08-26 23:58 1. 프로그래머스 Level 2. 점프와 순간 이동 https://school.programmers.co.kr/learn/courses/30/lessons/12980

---
+++ CodingTest-Java/src/main/java/programmers2/JumpAndTeleport.java
@@ -0,0 +1,33 @@
+package main.java.programmers2;
+
+/*
+ * 프로그래머스 Level 2. 점프와 순간 이동
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12980
+ *
+ * 생성일 : 2023-08-26
+ * 완료일 :
+ */
+public class JumpAndTeleport {
+
+    public static void main(String[] args) {
+        JumpAndTeleport sol = new JumpAndTeleport();
+
+        int N1 = 5;
+        System.out.println(sol.solution(N1));
+
+//        int N2 = 6;
+//        System.out.println(sol.solution(N2));
+//
+//        int N3 = 5000;
+//        System.out.println(sol.solution(N3));
+    }
+
+    private int solution(int n) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
