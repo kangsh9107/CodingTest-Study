b484379eaed90e56c04c3416fed4c1cc7e75d2db
KangSuHyeong <kangsh9107@naver.com>
2023-09-11 23:54:35 +0900

    2023-09-11 23:54 1. 프로그래머스 Level 2. 점프와 순간 이동 https://school.programmers.co.kr/learn/courses/30/lessons/12980

--- CodingTest-Java/src/main/java/programmers2/JumpAndTeleport.java
+++ CodingTest-Java/src/main/java/programmers2/JumpAndTeleport.java
@@ -24,11 +24,14 @@ public class JumpAndTeleport {
 
     private int solution(int n) {
         int answer = 0;
-        int jump = 0;
-        int teleport = 0;
 
-        while (n > 1) {
-
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+                answer++;
+            }
         }
 
         return answer;
